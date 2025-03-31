7607c7998cb4705c45389388dba06faaaa70bb57
sanika Salunkhe <salunkhesanika610@gmail.com>
2025-03-31 04:43:00 -0700

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'delta_app',
-  password: 'S@nika132007'
+  password: //password
 });
 
 // Function to generate a random user
